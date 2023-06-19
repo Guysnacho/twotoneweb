1fdb98deb8b67e8458275805b7c7cef5c94d8cb0
Samuel Adetunji <guysnacho2@gmail.com>
2023-06-19 17:56:39 -0500

    Init tt-web 🌱

---
+++ README.md
@@ -0,0 +1,52 @@
+# MVP
+
+## So boom
+
+We can start with giving everyone a song of the day and timeline.
+Songs of the day are eternal and persistent. After 100 they can be frozen.
+
+## Goals
+
+Things to knock out for a minimal viable product. Something that's good enough to give to people
+
+### Notebook - "Your" home
+
+  [ ] - Profile info
+  [ ] - Songs of the day
+  [ ] - Wallpaper
+    - customization
+  [ ] - ~~Bubbles~~ Stickies
+  [ ] - Playlists
+
+### Bulletin
+
+  [ ] - List of the week
+  [ ] - user's lists
+  [ ] - Bubbles of recently listened songs
+  [ ] - Currently listening song autoplays
+    - Car drives by w the speakers on 40
+
+Songs of the day can be edge functions although that's unnecessary.
+
+## Developing
+
+Once you've created a project and installed dependencies with `yarn`, start a development server:
+
+```bash
+yarn dev
+
+# or start the server and open the app in a new browser tab
+yarn dev -- --open
+```
+
+## Building
+
+To create a production version of your app:
+
+```bash
+yarn build
+```
+
+You can preview the production build with `yarn preview`.
+
+> To deploy your app, you may need to install an [adapter](https://kit.svelte.dev/docs/adapters) for your target environment.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,98 @@
+<script lang="ts">
+	import Logo from "$lib/logo.png";
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center flex flex-col items-center">
+		<h2 class="h2">Find music your way</h2>
+		<!-- Animated Logo -->
+		<figure>
+			<section class="img-bg" />
+			<img class="h-15 w-15" src={Logo} alt="TwoTone Logo" />
+		</figure>
+		<!-- / -->
+		<h2>TwoTone</h2>
+	</div>
+</div>
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center flex flex-col items-center">
+		<h2 class="h2">Find music your way</h2>
+		<!-- Animated Logo -->
+		<figure>
+			<section class="img-bg" />
+			<img class="h-15 w-15" src={Logo} alt="TwoTone Logo" />
+		</figure>
+		<!-- / -->
+		<h2>TwoTone</h2>
+	</div>
+</div>
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center flex flex-col items-center">
+		<h2 class="h2">Find music your way</h2>
+		<!-- Animated Logo -->
+		<figure>
+			<section class="img-bg" />
+			<img class="h-15 w-15" src={Logo} alt="TwoTone Logo" />
+		</figure>
+		<!-- / -->
+		<h2>TwoTone</h2>
+	</div>
+</div>
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center flex flex-col items-center">
+		<h2 class="h2">Find music your way</h2>
+		<!-- Animated Logo -->
+		<figure>
+			<section class="img-bg" />
+			<img class="h-15 w-15" src={Logo} alt="TwoTone Logo" />
+		</figure>
+		<!-- / -->
+		<h2>TwoTone</h2>
+	</div>
+</div>
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center flex flex-col items-center">
+		<h2 class="h2">Find music your way</h2>
+		<!-- Animated Logo -->
+		<figure>
+			<section class="img-bg" />
+			<img class="h-15 w-15" src={Logo} alt="TwoTone Logo" />
+		</figure>
+		<!-- / -->
+		<h2>TwoTone</h2>
+	</div>
+</div>
+
+<style lang="postcss">
+	figure {
+		@apply flex relative flex-col;
+	}
+	figure img,
+	.img-bg {
+		@apply w-64 h-64 md:w-80 md:h-80;
+	}
+	.img-bg {
+		@apply absolute z-[-1] rounded-full blur-[50px] transition-all;
+		animation: pulse 5s cubic-bezier(0, 0, 0, 0.5) infinite,
+			glow 5s linear infinite;
+	}
+	@keyframes glow {
+		0% {
+			@apply bg-primary-400/50;
+		}
+		33% {
+			@apply bg-secondary-400/50;
+		}
+		66% {
+			@apply bg-tertiary-400/50;
+		}
+		100% {
+			@apply bg-primary-400/50;
+		}
+	}
+	@keyframes pulse {
+		50% {
+			transform: scale(1.5);
+		}
+	}
+</style>
