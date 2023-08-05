3ae20079d97942f9b21eed8603d2b899e91f5bb5
Samuel Adetunji <guysnacho2@gmail.com>
2023-08-05 12:39:30 -0500

    Fix env variable instantiation

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,5 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './schema';
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from '$env/static/private';
 
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient<Database>(
+	process.env.SUPABASE_URL || '',
+	process.env.SUPABASE_ANON_KEY || ''
+);
