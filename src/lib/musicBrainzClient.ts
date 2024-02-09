09a4810d49b8cfcc780a9b1e15edf51e89c117ae
Samuel Adetunji <guysnacho2@gmail.com>
2024-02-09 03:06:41 -0600

    Add musicbrainz library

---
+++ src/lib/musicBrainzClient.ts
@@ -0,0 +1,8 @@
+import { MusicBrainzApi } from 'musicbrainz-api';
+
+export const mbApi = new MusicBrainzApi({
+	appName: 'TwoTone',
+	appVersion: '0.1.0',
+	appContactInfo: 'tunjiprod@gmail.com',
+	botAccount: {}
+});
