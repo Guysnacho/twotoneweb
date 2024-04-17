72437c545af36bc71e0c1f1b7eefc1c78375f6db
Samuel Adetunji <guysnacho2@gmail.com>
2024-04-16 22:37:38 -0500

    Add auth token procedure

--- src/lib/server/trpc/t.ts
+++ src/lib/server/trpc/t.ts
@@ -1,4 +1,6 @@
+import { SEARCH_SECRET } from '$env/static/private';
 import { TRPCError, initTRPC } from '@trpc/server';
+import { stringify } from 'querystring';
 import SuperJSON from 'superjson';
 import type { Context } from './context';
 
@@ -19,6 +21,45 @@ export const superSecretProc = t.procedure.use(
 			ctx: {
 				// infers the `session` as non-nullable
 				session: { ...ctx.session, user: ctx.session.user }
+			}
+		});
+	})
+);
+
+export const spicySearchProc = t.procedure.use(
+	t.middleware(async ({ ctx, next }) => {
+		if (!ctx.session || !ctx.session.user) {
+			throw new TRPCError({ code: 'UNAUTHORIZED' });
+		}
+		
+		// Fetch spotify auth token
+		const authorization = await fetch('https://accounts.spotify.com/api/token', {
+			method: 'POST',
+			headers: {
+				Authorization: 'Basic ' + SEARCH_SECRET,
+				'Content-Type': 'application/x-www-form-urlencoded',
+				'Cache-Control': 'max-age=3600'
+			},
+			body: stringify({
+				grant_type: 'client_credentials'
+			})
+		});
+
+		if (!authorization.ok) {
+			throw new TRPCError({
+				code: 'FORBIDDEN',
+				message: 'Something went wrong while fetching auth token, try again in a minute.'
+			});
+		}
+
+		const searchToken = (await authorization.json()).access_token;
+		console.log('Successfully fetched token - %s', searchToken);
+		return next({
+			ctx: {
+				// infers the `session` as non-nullable
+				session: { ...ctx.session, user: ctx.session.user },
+				// enrich context with auth token
+				searchToken
 			}
 		});
 	})
