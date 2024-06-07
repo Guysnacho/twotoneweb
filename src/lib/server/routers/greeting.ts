f490aa285126af8f6ac679c4501d011cf14123cc
Samuel Adetunji <guysnacho2@gmail.com>
2024-06-07 17:01:30 -0500

    Add time to date query

--- src/lib/server/routers/greeting.ts
+++ src/lib/server/routers/greeting.ts
@@ -1,3 +1,4 @@
+import { z } from 'zod';
 import { publicProc, router } from '../trpc/t';
 
 function getTimeOfDay() {
@@ -15,10 +16,37 @@ function getTimeOfDay() {
 	}
 }
 
+function getTimeOfWithDate(date: Date) {
+	const curHr = date.getHours();
+
+	if (curHr < 4) {
+		return 'night';
+	} else if (curHr < 12) {
+		return 'morning';
+	} else if (curHr < 18) {
+		return 'afternoon';
+	} else {
+		return 'night';
+	}
+}
+
 export const greetingRouter = router({
-	greet: publicProc.query(async ({ ctx: { session, supabase } }) => {
+	greet: publicProc.query(async ({ ctx: { session } }) => {
 		return `Good ${getTimeOfDay()}${
 			session?.user.user_metadata.username ? ' ' + session?.user.user_metadata.username : ''
 		}!`;
-	})
+	}),
+	greetWTime: publicProc
+		.input(
+			z.object({
+				date: z.date({}).describe('text used to search for song')
+			})
+		)
+		.query(async ({ ctx: { session }, input: { date } }) => {
+			console.debug('Greeting event with date');
+			console.debug(date);
+			return `Good ${getTimeOfWithDate(date)}${
+				session?.user.user_metadata.username ? ' ' + session?.user.user_metadata.username : ''
+			}!`;
+		})
 });
