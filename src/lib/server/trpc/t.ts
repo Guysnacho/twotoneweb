4f2751f619f83ce0674f9ec7d567afb4f1ca5d61
Samuel Adetunji <guysnacho2@gmail.com>
2024-06-19 12:58:36 -0500

    Add token generation

--- src/lib/server/trpc/t.ts
+++ src/lib/server/trpc/t.ts
@@ -1,9 +1,10 @@
-import { SEARCH_SECRET } from '$env/static/private';
+import { APPLE_SERVICE_KEY, ISS_ID, KEY_ID, SEARCH_SECRET } from '$env/static/private';
+import { fetchSavedToken, saveToken } from '$lib/musicHelper';
 import { TRPCError, initTRPC } from '@trpc/server';
+import * as jose from 'jose';
 import { stringify } from 'querystring';
 import SuperJSON from 'superjson';
 import type { Context } from './context';
-
 
 export const t = initTRPC.context<Context>().create({
 	transformer: SuperJSON
@@ -60,6 +61,61 @@ export const spicySearchProc = t.procedure.use(
 				session: { ...ctx.session, user: ctx.session.user },
 				// enrich context with auth token
 				searchToken
+			}
+		});
+	})
+);
+
+export const appleSearchProc = t.procedure.use(
+	t.middleware(async ({ ctx, next }) => {
+		if (!ctx.session || !ctx.session.user) {
+			throw new TRPCError({ code: 'UNAUTHORIZED' });
+		}
+
+		const token = await fetchSavedToken();
+
+		// If we have an unexpired token
+		if (token !== undefined || token !== '') {
+			return next({
+				ctx: {
+					// infers the `session` as non-nullable
+					session: { ...ctx.session, user: ctx.session.user },
+					// enrich context with auth token
+					appleSearchToken: token
+				}
+			});
+		}
+
+		const issued_at = new Date().getMilliseconds();
+		const expires_at = new Date().getMilliseconds() + 1000 * 60; //* 60 * 24,
+		const appleKey = await jose.importPKCS8(APPLE_SERVICE_KEY, 'es256');
+		console.debug('Successfully built Apple Music Key - %s', appleKey.type);
+
+		// Build
+		const jwt = await new jose.EncryptJWT({
+			iss: ISS_ID,
+			iat: issued_at,
+			// (Milli -> second) -> 1 min -> 1 hour -> 24 hours
+			exp: expires_at,
+			origin: [
+				'http://localhost',
+				'http://localhost:5173',
+				'https://twotone.app',
+				'https://www.twotone.app'
+			]
+		})
+			.setProtectedHeader({ kid: KEY_ID, alg: 'ES256', enc: 'ES256' })
+			.encrypt(appleKey);
+
+		console.debug('Successfully built developer token - %s', jwt);
+
+		await saveToken(jwt, issued_at, expires_at);
+		return next({
+			ctx: {
+				// infers the `session` as non-nullable
+				session: { ...ctx.session, user: ctx.session.user },
+				// enrich context with auth token
+				appleSearchToken: jwt
 			}
 		});
 	})
