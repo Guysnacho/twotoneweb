cae396f6bdff3015c9a8c85401284d8181ecccdd
Samuel Adetunji <guysnacho2@gmail.com>
2024-06-21 16:20:09 -0500

    Add supabase ssr, update trpc context and add supabase to page layout context

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,5 +1,6 @@
+import { SUPABASE_ANON_KEY } from '$env/static/private';
+import { PUBLIC_SUPABASE_URL } from '$env/static/public';
 import { createClient } from '@supabase/supabase-js';
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from '$env/static/private';
 import type { Database } from './schema';
 
-export const supabase = createClient<Database>(SUPABASE_URL || '', SUPABASE_ANON_KEY || '');
+export const supabase = createClient<Database>(PUBLIC_SUPABASE_URL || '', SUPABASE_ANON_KEY || '');
