e962f5efe2a2daea00b35d4b4cad2a4cec9b11be
Samuel Adetunji <guysnacho2@gmail.com>
2024-06-23 01:26:12 -0500

    Revert "Revert "Add booth page and form for login""
    This reverts commit d733e68535bfa127f0bf950b5e8a5dfb3e71fc42.

---
+++ src/routes/admin/booth/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { LayoutData } from './$types';
+	import { goto } from '$app/navigation';
+
+	export let data: LayoutData;
+	if (data.session === null) {
+		data.supabase.auth.signOut().finally(() => goto('/admin'));
+	}
+</script>
+
+<slot />
