0ea7702365633df6947761af31ef6f44c66289df
Samuel Adetunji <guysnacho2@gmail.com>
2024-07-18 00:36:37 -0500

    Bring back delete endpoint

--- src/routes/api/auth/+server.ts
+++ src/routes/api/auth/+server.ts
@@ -67,61 +67,61 @@ export const GET = (() => {
 // 		});
 // }) satisfies RequestHandler;
 
-// /**
-//  * @deprecated
-//  * @description Handle supabase user deletion
-//  */
-// export const DELETE = (async ({ request, url }) => {
-// 	console.debug('Delete request recieved');
+/**
+ * Whoops, this actually wasn't depracated, gotta secure this though.
+ * @description Handle supabase user deletion
+ */
+export const DELETE = (async ({ request, url }) => {
+	console.debug('Delete request recieved');
 
-// 	const id = isValidDeleteRequest(request, url);
+	const id = isValidDeleteRequest(request, url);
 
-// 	if (!id) {
-// 		throw error(HttpCodes.BADREQUEST, {
-// 			code: HttpCodes.BADREQUEST,
-// 			message: 'Invalid delete request'
-// 		});
-// 	}
+	if (!id) {
+		throw error(HttpCodes.BADREQUEST, {
+			code: HttpCodes.BADREQUEST,
+			message: 'Invalid delete request'
+		});
+	}
 
-// 	return supabase.auth.admin
-// 		.deleteUser(id)
-// 		.then(async (res) => {
-// 			if (res.error?.status) {
-// 				throw error(res.error.status || HttpCodes.BADREQUEST, {
-// 					code: res.error.status || HttpCodes.BADREQUEST,
-// 					message: res.error.message
-// 				});
-// 			} else if (res.data.user) {
-// 				return json(
-// 					{
-// 						id,
-// 						deleted_at: new Date()
-// 					},
-// 					{ status: 200, statusText: 'Account Deleted' }
-// 				);
-// 			} else {
-// 				throw error(HttpCodes.INTERNALERROR, {
-// 					code: HttpCodes.INTERNALERROR,
-// 					message: 'Error during account deletion, try again later'
-// 				});
-// 			}
-// 		})
-// 		.catch((err) => {
-// 			console.debug(err);
+	return supabase.auth.admin
+		.deleteUser(id)
+		.then(async (res) => {
+			if (res.error?.status) {
+				throw error(res.error.status || HttpCodes.BADREQUEST, {
+					code: res.error.status || HttpCodes.BADREQUEST,
+					message: res.error.message
+				});
+			} else if (res.data.user) {
+				return json(
+					{
+						id,
+						deleted_at: new Date()
+					},
+					{ status: 200, statusText: 'Account Deleted' }
+				);
+			} else {
+				throw error(HttpCodes.INTERNALERROR, {
+					code: HttpCodes.INTERNALERROR,
+					message: 'Error during account deletion, try again later'
+				});
+			}
+		})
+		.catch((err) => {
+			console.debug(err);
 
-// 			throw error(HttpCodes.INTERNALERROR, {
-// 				code: HttpCodes.INTERNALERROR,
-// 				message: 'Error during account deletion, try again later'
-// 			});
-// 		});
-// }) satisfies RequestHandler;
+			throw error(HttpCodes.INTERNALERROR, {
+				code: HttpCodes.INTERNALERROR,
+				message: 'Error during account deletion, try again later'
+			});
+		});
+}) satisfies RequestHandler;
 
-// /**
-//  * @deprecated
-//  * Validates sign up request
-//  * @param request
-//  * @returns result of auth validation
-//  */
+/**
+ * @deprecated
+ * Validates sign up request
+ * @param request
+ * @returns result of auth validation
+ */
 // const isValidAuthRequest = async (request: Request) => {
 // 	const payload = await request.json();
 // 	if (
@@ -136,13 +136,13 @@ export const GET = (() => {
 // 	} else return payload;
 // };
 
-// /**
-//  * Validates delete request
-//  * @param request
-//  * @returns result of auth validation
-//  */
-// const isValidDeleteRequest = (request: Request, url: URL) => {
-// 	if (request.headers.get('x-trpc-source') !== 'expo-react' || !url.searchParams.has('id'))
-// 		return undefined;
-// 	return url.searchParams.get('id') as string;
-// };
+/**
+ * Validates delete request
+ * @param request
+ * @returns result of auth validation
+ */
+const isValidDeleteRequest = (request: Request, url: URL) => {
+	if (request.headers.get('x-trpc-source') !== 'expo-react' || !url.searchParams.has('id'))
+		return undefined;
+	return url.searchParams.get('id') as string;
+};
